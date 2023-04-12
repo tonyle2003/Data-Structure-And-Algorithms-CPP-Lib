63396fca0473b060fc036b6c8427bbd5e505fff0
tonyle2003 <laptrinhcungtony@gmail.com>
2023-04-12 23:49:16 +0700

    12/04/2023 add ActivitySelection.cpp

--- lib/algorithms/greedy/ActivitySelection.cpp
+++ lib/algorithms/greedy/ActivitySelection.cpp
@@ -20,26 +20,28 @@ class Activity {
         X finish_time;
 
     public:
-        friend istream& operator >> (istream&, Activity<X>&);
-        friend ostream& operator << (ostream&, Activity<X>);
-        bool operator < (Activity<X>);
+        template<typename Y>
+        friend istream& operator >> (istream&, Activity<Y>&);
+        template<typename Y>
+        friend ostream& operator << (ostream&, const Activity<Y>&);
+        bool operator < (const Activity<X>&) const;
         Activity();
         Activity(X, X, X);
-        X getIndex();
-        X getStartTime();
-        X getFinishTime();
+        X getIndex() const;
+        X getStartTime() const;
+        X getFinishTime() const;
         void setIndex(X);
         void setStartTime(X);
         void setFinishTime(X);
 };
 
-template<typename X>
-istream& operator >> (istream& input, Activity<X>& activity) {
+template<typename Y>
+istream& operator >> (istream& input, Activity<Y>& activity) {
 
     static_assert(
-        is_integral<X>::value
-        && !is_same<X, char>::value
-        && !is_same<X, bool>::value,
+        is_integral<Y>::value
+        && !is_same<Y, char>::value
+        && !is_same<Y, bool>::value,
         "Error data type."
     );
 
@@ -47,13 +49,13 @@ istream& operator >> (istream& input, Activity<X>& activity) {
     return input;
 }
 
-template<typename X>
-ostream& operator << (ostream& output, Activity<X> activity) {
+template<typename Y>
+ostream& operator << (ostream& output, const Activity<Y>& activity) {
 
     static_assert(
-        is_integral<X>::value
-        && !is_same<X, char>::value
-        && !is_same<X, bool>::value,
+        is_integral<Y>::value
+        && !is_same<Y, char>::value
+        && !is_same<Y, bool>::value,
         "Error data type."
     );
 
@@ -62,7 +64,7 @@ ostream& operator << (ostream& output, Activity<X> activity) {
 }
 
 template<typename X>
-bool Activity<X>::operator < (Activity<X> activity) {
+bool Activity<X>::operator < (const Activity<X>& activity) const {
     return this->finish_time < activity.finish_time;
 }
 
@@ -81,17 +83,17 @@ Activity<X>::Activity(X index, X start_time, X finish_time) {
 }
 
 template<typename X>
-X Activity<X>::getIndex() {
+X Activity<X>::getIndex() const {
     return this->index;
 }
 
 template<typename X>
-X Activity<X>::getStartTime() {
+X Activity<X>::getStartTime() const {
     return this->start_time;
 }
 
 template<typename X>
-X Activity<X>::getFinishTime() {
+X Activity<X>::getFinishTime() const {
     return this->finish_time;
 }
 
@@ -108,4 +110,49 @@ void Activity<X>::setStartTime(X start_time) {
 template<typename X>
 void Activity<X>::setFinishTime(X finish_time) {
     this->finish_time = finish_time;
+}
+
+template<typename T>
+class ActivitySelection {
+
+    static_assert(
+        is_integral<T>::value
+        && !is_same<T, char>::value
+        && !is_same<T, bool>::value,
+        "Error data type."
+    );
+
+    public:
+        static void selectedActivities(Activity<T>*, T);
+        static T getMaxActivities(Activity<T>*, T);
+};
+
+template<typename T>
+void ActivitySelection<T>::selectedActivities(Activity<T>* activity, T size) {
+    sort(activity, activity + size);
+    T i, j;
+    i = 0;
+    cout << activity[i].getIndex() << " ";
+    for (j = 1; j < size; j++) {
+        if (activity[j].getStartTime() >= activity[i].getFinishTime()) {
+            cout << activity[j].getIndex() << " ";
+            i = j;
+        }
+    }
+    cout << endl;
+}
+
+template<typename T>
+T ActivitySelection<T>::getMaxActivities(Activity<T>* activity, T size) {
+    sort(activity, activity + size);
+    T i, j;
+    i = 0;
+    T count = 1;
+    for (j = 1; j < size; j++) {
+        if (activity[j].getStartTime() >= activity[i].getFinishTime()) {
+            count++;
+            i = j;
+        }
+    }
+    return count;
 }
